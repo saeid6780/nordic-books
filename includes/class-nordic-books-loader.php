8670dc44f876fe00a1696a5ca4deada86a350e89
Saeid SedighZadeh <ssadighzade@gmail.com>
2025-09-06 19:48:01 +0330

    feat - add add_shortcode loader

--- includes/class-nordic-books-loader.php
+++ includes/class-nordic-books-loader.php
@@ -41,6 +41,15 @@ class Nordic_Books_Loader {
 	 */
 	protected $filters;
 
+    /**
+     * The array of shortcodes registered with WordPress.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      array $shortcodes The shortcodes registered with WordPress to fire when the plugin loads.
+     */
+    protected $shortcodes;
+
 	/**
 	 * Initialize the collections used to maintain the actions and filters.
 	 *
@@ -50,7 +59,7 @@ class Nordic_Books_Loader {
 
 		$this->actions = array();
 		$this->filters = array();
-
+        $this->shortcodes = array();
 	}
 
 	/**
@@ -80,6 +89,22 @@ class Nordic_Books_Loader {
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
 	}
+
+    /**
+     * Add a new shortcode to the collection to be registered with WordPress.
+     *
+     * @param string $hook The name of the WordPress filter that is being registered.
+     * @param object $component A reference to the instance of the object on which the filter is defined.
+     * @param string $callback The name of the function definition on the $component.
+     * @param int    $priority Optional. The priority at which the function should be fired. Default is 10.
+     * @param int    $accepted_args Optional. The number of arguments that should be passed to the $callback. Default is 1
+     *
+     * @since    1.0.0
+     */
+    public function add_shortcode ( $hook, $component, $callback, $priority = 10, $accepted_args = 1 )
+    {
+        $this->shortcodes = $this->add( $this->shortcodes, $hook, $component, $callback, $priority, $accepted_args );
+    }
 
 	/**
 	 * A utility function that is used to register the actions and hooks into a single
@@ -123,6 +148,9 @@ class Nordic_Books_Loader {
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
 		}
+
+        foreach ( $this->shortcodes as $hook )
+            add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
 
 	}
 
